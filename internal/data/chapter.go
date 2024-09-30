1261a193d779f6a7eeb0d44fe8f55a1b3cf663be
Juanmagc99 <juanma101099@gmail.com>
2024-09-30 19:26:16 +0200

    Insert for chapter

--- internal/data/chapter.go
+++ internal/data/chapter.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"time"
 
@@ -12,6 +13,7 @@ type Chapter struct {
 	GnovelID  int64     `json:"gnovel_id"`
 	Number    int       `json:"number"`
 	PageCount int       `json:"page_count"`
+	FilePath  string    `json:"file_path"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
@@ -23,4 +25,19 @@ func ValidateChapter(v *validator.Validator, chp *Chapter) {
 	v.Check(chp.Number >= 0, "number", "Number of chapter cant be negative")
 
 	v.Check(chp.PageCount >= 1, "page_count", "A chapter must have at least one page")
+}
+
+func (m ChapterModel) Insert(chapter *Chapter) error {
+	query := `
+		INSERT INTO chapters (gnovelid, number, pagecount, filpath)
+		VALUES ($1,$2,$3,$4)
+		RETURNING id, created_at
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	args := []any{chapter.GnovelID, chapter.Number, chapter.PageCount, chapter.FilePath}
+
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&chapter.ID, &chapter.CreatedAt)
 }
