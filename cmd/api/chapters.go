1a4a0f3f2d8336005a84fb509bbcd17150297b33
Juanmagc99 <juanma101099@gmail.com>
2024-10-01 22:32:31 +0200

    Put for chapters

--- cmd/api/chapters.go
+++ cmd/api/chapters.go
@@ -109,7 +109,85 @@ func (app *application) createChapterHandler(w http.ResponseWriter, r *http.Requ
 
 // Handler for PUT /v1/gnovels/:id/chapter/:number
 func (app *application) updateChapterHandler(w http.ResponseWriter, r *http.Request) {
+	const maxUploadSize = 30 * 1024 * 1024 // 30MB
 
+	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+
+	err := r.ParseMultipartForm(maxUploadSize)
+	if err != nil {
+		app.badRequestResponse(w, r, fmt.Errorf("the file is too heavy"))
+		return
+	}
+
+	id, err := app.readIntParam(r, "id")
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	number, err := app.readIntParam(r, "number")
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Recoger el archivo subido (campo "file")
+	file, fileheader, err := r.FormFile("file")
+	if err != nil {
+		app.badRequestResponse(w, r, fmt.Errorf("unable to obtain archive: %v", err))
+		return
+	}
+
+	defer file.Close()
+
+	filename := fileheader.Filename
+	if !strings.HasSuffix(filename, ".cbz") {
+		app.badRequestResponse(w, r, fmt.Errorf("only archives with .cbz are allowed"))
+	}
+
+	chapter := &data.Chapter{
+		GnovelID: id,
+		Number:   int(number),
+	}
+
+	v := validator.New()
+	if data.ValidateChapter(v, chapter, app.models.Gnovels); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.deleteFile(id, int(number))
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.models.Chapters.Delete(id, int(number))
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	filepath, err := app.uploadChapter(chapter.GnovelID, chapter.Number, file)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+	}
+
+	chapter.FilePath = filepath
+
+	err = app.models.Chapters.Insert(chapter)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/chapters/%d", chapter.ID))
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"chapter": chapter}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // Handler for DELETE /v1/gnovels/:id/chapter/:number
