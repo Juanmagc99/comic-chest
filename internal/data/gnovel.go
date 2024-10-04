9dff622993661bfc3abc5821dc5d9ed642eaaa23
Juanmagc99 <juanma101099@gmail.com>
2024-10-04 19:56:55 +0200

    add filter

--- internal/data/gnovel.go
+++ internal/data/gnovel.go
@@ -70,12 +70,14 @@ func (m GnovelModel) GetAll(title string, genres []string, filters Filters) ([]*
 	query := `
 	SELECT *
 	FROM gnovels
+	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+	AND (genres @> $2 OR $2 = '{}')
 	ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
