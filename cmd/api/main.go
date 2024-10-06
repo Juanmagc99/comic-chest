c5dbc2ed6e6bb50d67e00286c3872f2aea5aba77
Juanmagc99 <juanma101099@gmail.com>
2024-10-06 17:15:24 +0200

    setting mail sender

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,9 +6,12 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strconv"
 	"time"
 
+	"github.com/joho/godotenv"
 	"juanmagc99.comic-chest/internal/data"
+	"juanmagc99.comic-chest/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -19,25 +22,47 @@ type config struct {
 	db   struct {
 		dsn string
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
 
 	var cfg config
 
+	godotenv.Load()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://comichest:1234@localhost/comichest?sslmode=disable", "PostgreSQL DSN")
 
-	flag.Parse()
+	smtpport, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	flag.StringVar(&cfg.smtp.host, "smtp-host", os.Getenv("SMTP_HOST"), "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", smtpport, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", os.Getenv("SMTP_USERNAME"), "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", os.Getenv("SMTP_PASSWORD"), "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", os.Getenv("SMTP_SENDER"), "SMTP sender")
+
+	flag.Parse()
 
 	db, err := openDB(cfg)
 	if err != nil {
@@ -49,6 +74,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.server()
--- go.mod
+++ go.mod
@@ -7,4 +7,9 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require golang.org/x/crypto v0.28.0 // indirect
+require (
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
